d7085b991ff037fce25b636826cf6f021a48be46
Munthazeer <140618137+Munthazeer@users.noreply.github.com>
2023-07-26 18:45:59 +0530

    Update and rename FrameProcess.java to HelloCities.java

---
+++ HelloCities.java
@@ -0,0 +1,42 @@
+// package Selenium;
+
+// import org.openqa.selenium.By;
+// import org.openqa.selenium.WebDriver;
+// import org.openqa.selenium.WebElement;
+// import org.openqa.selenium.chrome.ChromeDriver;
+
+public class HelloCities {
+
+	public static void main(String[] args) throws InterruptedException {
+		System.out.println("Nagercoil");
+		System.out.println("Chennai");
+		// // TODO Auto-generated method stub
+		// System.setProperty("webdriver.chrome.driver", "E:\\Selenium\\Chrome\\chromedriver.exe");
+		// WebDriver driver = new ChromeDriver();
+		// driver.get("https://www.leafground.com/dashboard.xhtml");
+		// Thread.sleep(2000);
+		// WebElement frame = driver.findElement(By.id("menuform:j_idt39"));
+		// frame.click();
+		// Thread.sleep(2000);
+		// WebElement frameselect = driver.findElement(By.id("menuform:m_frame"));
+		// frameselect.click();
+		// driver.switchTo().frame(0);
+		// WebElement firstframebutton = driver.findElement(By.id("click"));
+		// firstframebutton.click();
+		// String firstvalue = firstframebutton.getText();
+		// System.out.println(firstvalue);
+		
+		// driver.switchTo().defaultContent();
+		// driver.switchTo().frame("frame2");
+//		WebElement frameselect = driver.findElement(By.id("menuform:m_frame"));
+//		frameselect.click();
+//		driver.switchTo().frame(0);
+		// WebElement secondframebutton = driver.findElement(By.id("click"));
+		// secondframebutton.click();
+		// String secondvalue = firstframebutton.getText();
+		// System.out.println(secondvalue);
+		
+		
+	}
+
+}
